1a13c0970b385a175ce8ddd9e5e1ad9de10d6971
Alex Pitzel <pitzelalex@gmail.com>
2023-04-11 15:39:54 -0400

    Add relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_11_192433) do
+ActiveRecord::Schema.define(version: 2023_04_11_192746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.bigint "tea_id"
+    t.bigint "customer_id"
+    t.string "title"
+    t.integer "price"
+    t.string "status"
+    t.string "frequency"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_subscriptions_on_customer_id"
+    t.index ["tea_id"], name: "index_subscriptions_on_tea_id"
+  end
 
   create_table "teas", force: :cascade do |t|
     t.string "title"
@@ -24,4 +46,6 @@ ActiveRecord::Schema.define(version: 2023_04_11_192433) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "subscriptions", "customers"
+  add_foreign_key "subscriptions", "teas"
 end
